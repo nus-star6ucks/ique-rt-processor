316be984b3f94586b8c69979ced76dfa016a529f
xz1996 <xz.cn@outlook.com>
2023-05-04 19:20:33 +0800

    enable cross origin

--- src/main/java/com/iqueue/dataflow/rtprocessor/RtProcessorApplication.java
+++ src/main/java/com/iqueue/dataflow/rtprocessor/RtProcessorApplication.java
@@ -11,5 +11,4 @@ public class RtProcessorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RtProcessorApplication.class, args);
 	}
-
 }
--- src/main/java/com/iqueue/dataflow/rtprocessor/controller/RTController.java
+++ src/main/java/com/iqueue/dataflow/rtprocessor/controller/RTController.java
@@ -9,10 +9,12 @@ import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQuerySer
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
+@CrossOrigin
 @Controller
 public class RTController {
 
